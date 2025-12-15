7b7d32ad601c901f20a27d63e5fd4c754528d1fb
Shinra.. <Adams-Ijachi>
2025-12-16 00:55:29 +0100

    Refactor range processing and add divisor caching for improved performance

--- day2.php
+++ day2.php
@@ -1,71 +1,93 @@
 <?php
+ini_set('memory_limit', '2G');
 
 $ranges = explode(",",file_get_contents('./ranges.txt'));
 
 $rangeTotal = 0;
 
-// Step 1
+// // // Step 1
 
-// foreach($ranges as $range){
-//     [$lower, $upper] = explode("-", $range);
-//     // Process each range as needed
-//     for($i = (int)$lower; $i <= (int)$upper; $i++){
-//         // if the number is count of number is not even the continue
-//         $numberLength = (int)strlen($i) ;
-//         if($numberLength % 2 != 0) continue;
+// // foreach($ranges as $range){
+// //     [$lower, $upper] = explode("-", $range);
+// //     // Process each range as needed
+// //     for($i = (int)$lower; $i <= (int)$upper; $i++){
+// //         // if the number is count of number is not even the continue
+// //         $numberLength = (int)strlen($i) ;
+// //         if($numberLength % 2 != 0) {
 
-//         $firstHalf = substr($i, 0, $numberLength/2);
-//         $secondHalf = substr($i, $numberLength/2);
-//         if($firstHalf ===  $secondHalf){
-//             $rangeTotal = $rangeTotal + $i;
-//         }
-//     }
-// }
+// //             // handle odd length numbers
+// //             continue;
 
-// echo $rangeTotal ."\n";
+// //         };
 
+// //         $firstHalf = substr($i, 0, $numberLength/2);
+// //         $secondHalf = substr($i, $numberLength/2);
+// //         if($firstHalf ===  $secondHalf){
+// //             $rangeTotal = $rangeTotal + $i;
+// //         }
+// //     }
+// // }
 
-function findValue($array)
-{
-    $currentNumber = $array[0];
+// // echo $rangeTotal ."\n";
 
-    if(count($array)< 2){
-        return $array[0];
-    }
-
-    // find first occurance
-    $index = 1;
-    while (  $array[$index] != $currentNumber ){
-
-        if($index >= count ($array) -1 ){
-            return false;
-        }
-        $index++;
-    }
-    $cutout = array_slice($array, $index);
-
-    findValue($cutout);
-
-    return $array;
-}
-
-$numberTotal = [];
-$values =[];
 
 foreach($ranges as $range){
     [$lower, $upper] = explode("-", $range);
     // Process each range as needed
     for($i = (int)$lower; $i <= (int)$upper; $i++){
-        
-        $arrayOfNumber = str_split($i);
-        
-        $node = findValue($arrayOfNumber);
-        if(!$node){
-            continue;
+        // if the number is count of number is not even the continue
+        $numberLength = (int)strlen($i) ;
+      
+        $splitNumbers = findDivisiors($numberLength);
+
+        foreach($splitNumbers as $splitNumber) {
+            $arrays = str_split($i, $splitNumber);
+            $allMatch = true;
+
+            for($j=1; $j < count($arrays); $j++){
+                if($arrays[0] !== $arrays[$j]){
+                    $allMatch = false;  // Changed: set flag instead of break
+                    break;
+                }
+            }
+            if($allMatch && count($arrays) > 1) {  // count > 1 ensures it's actually repeated
+                $rangeTotal = $rangeTotal + $i;
+                break;  // NEW: stop checking other divisors once we find a match
+            }
+
+
+
         }
 
-        $values[] = $node;
-
-        
+       
     }
+}
+echo $rangeTotal ."\n";
+
+
+
+
+
+
+
+
+function findDivisiors($num ){
+    $divisors = [];
+    for($i = 1; $i <= sqrt($num); $i++){
+        if($num % $i == 0){
+            $divisors[] = $i;
+            if($i != $num / $i){  // NEW: avoid duplicates for perfect squares
+                $divisors[] = $num / $i;
+            }
+        }
+    }
+
+    sort($divisors);
+    $result = [];
+    foreach($divisors as $divisor){
+        if($divisor < $num){
+            $result[] = $divisor;
+        }
+    }
+    return $result;
 }
